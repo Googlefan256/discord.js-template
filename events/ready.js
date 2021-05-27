d799c85932e75b23a4fad76363953ec6fc0f1a87
Googlefan256 <55381698+Googlefan256@users.noreply.github.com>
2021-05-27 14:27:21 +0900

    Delete ready.js

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		console.log('-----------');
-  		console.log(`started bot as ${client.user.tag}`)
-  		console.log('-----------');
-	},
-};
